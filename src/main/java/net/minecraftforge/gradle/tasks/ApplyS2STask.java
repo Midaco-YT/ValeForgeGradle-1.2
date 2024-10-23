5654fe18f9718d0ca8d1760d7291f287f1f7f4f7
Midaco-YT <86883006+Midaco-YT@users.noreply.github.com>
2024-10-23 18:28:20 +0200

    Modify some code

--- src/main/java/net/minecraftforge/gradle/tasks/ApplyS2STask.java
+++ src/main/java/net/minecraftforge/gradle/tasks/ApplyS2STask.java
@@ -1,64 +1,58 @@
 package net.minecraftforge.gradle.tasks;
 
-import com.google.common.base.Charsets;
-import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
-import com.google.common.base.Throwables;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.io.Files;
+import net.minecraftforge.gradle.GradleVersionUtils;
+import net.minecraftforge.gradle.ProjectBuildDirHelper;
 import net.minecraftforge.gradle.SequencedInputSupplier;
 import net.minecraftforge.gradle.common.Constants;
 import net.minecraftforge.gradle.delayed.DelayedFile;
 import net.minecraftforge.srg2source.rangeapplier.RangeApplier;
-import net.minecraftforge.srg2source.util.io.FolderSupplier;
-import net.minecraftforge.srg2source.util.io.InputSupplier;
-import net.minecraftforge.srg2source.util.io.OutputSupplier;
-import net.minecraftforge.srg2source.util.io.ZipInputSupplier;
-import net.minecraftforge.srg2source.util.io.ZipOutputSupplier;
+import net.minecraftforge.srg2source.util.io.*;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.FileCollection;
-import org.gradle.api.tasks.InputFile;
-import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.tasks.Optional;
-import org.gradle.api.tasks.OutputFiles;
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.*;
 
+import javax.inject.Inject;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
-public class ApplyS2STask extends DefaultTask
-{
-    @InputFiles
-    private final List<Object> srg = new LinkedList<Object>();
+@CacheableTask
+public class ApplyS2STask extends DefaultTask {
+    private final List<Object> srg = new LinkedList<>();
 
-    @Optional
-    @InputFiles
-    private final List<Object> exc = new LinkedList<Object>();
+    private final List<Object> exc = new LinkedList<>();
+    private final File buildDir = ProjectBuildDirHelper.getBuildDir(getProject());
 
     @InputFile
+    @PathSensitive(PathSensitivity.ABSOLUTE)
     private DelayedFile rangeMap;
 
     @Optional
     @InputFile
+    @PathSensitive(PathSensitivity.ABSOLUTE)
     private DelayedFile excModifiers;
 
     // stuff defined on the tasks..
-    private final List<DelayedFile> in = new LinkedList<DelayedFile>();
+    private final List<DelayedFile> in = new LinkedList<>();
     private DelayedFile out;
 
+    public ApplyS2STask() {
+        this.getOutputs().doNotCacheIf("Gradle Version is Old", e -> GradleVersionUtils.isBefore("6.0"));
+    }
+
     @TaskAction
-    public void doTask() throws IOException
-    {
+    public void doTask() throws IOException {
         List<File> ins = getIn();
         File out = getOut();
         File rangemap = getRangeMap();
-        File rangelog = File.createTempFile("rangelog", ".txt", this.getTemporaryDir());
+        Path rangelog = Files.createTempFile(this.getTemporaryDir().toPath(), "rangelog", ".txt");
         FileCollection srg = getSrgs();
         FileCollection exc = getExcs();
 
@@ -66,13 +60,10 @@ public class ApplyS2STask extends DefaultTask
 
         if (ins.size() == 0)
             return; // no input.
-        else if (ins.size() == 1)
-        {
+        else if (ins.size() == 1) {
             // just 1 supplier.
             inSup = getInput(ins.get(0));
-        }
-        else
-        {
+        } else {
             // multinput
             inSup = new SequencedInputSupplier();
             for (File f : ins)
@@ -85,8 +76,7 @@ public class ApplyS2STask extends DefaultTask
         else
             outSup = getOutput(out);
 
-        if (getExcModifiers() != null)
-        {
+        if (getExcModifiers() != null) {
             getLogger().lifecycle("creating default param names");
             exc = generateDefaultExc(getExcModifiers(), exc, srg);
         }
@@ -99,40 +89,32 @@ public class ApplyS2STask extends DefaultTask
         outSup.close();
     }
 
-    private InputSupplier getInput(File f) throws IOException
-    {
+    private InputSupplier getInput(File f) throws IOException {
         if (f.isDirectory())
             return new FolderSupplier(f);
-        else if (f.getPath().endsWith(".jar") || f.getPath().endsWith(".zip"))
-        {
+        else if (f.getPath().endsWith(".jar") || f.getPath().endsWith(".zip")) {
             ZipInputSupplier supp = new ZipInputSupplier();
             supp.readZip(f);
             return supp;
-        }
-        else
+        } else
             throw new IllegalArgumentException("Can only make suppliers out of directories and zips right now!");
     }
 
-    private OutputSupplier getOutput(File f) throws IOException
-    {
+    private OutputSupplier getOutput(File f) throws IOException {
         if (f.isDirectory())
             return new FolderSupplier(f);
-        else if (f.getPath().endsWith(".jar") || f.getPath().endsWith(".zip"))
-        {
+        else if (f.getPath().endsWith(".jar") || f.getPath().endsWith(".zip")) {
             return new ZipOutputSupplier(f);
-        }
-        else
+        } else
             throw new IllegalArgumentException("Can only make suppliers out of directories and zips right now!");
     }
 
-    private void applyRangeMap(InputSupplier inSup, OutputSupplier outSup, FileCollection srg, FileCollection exc, File rangeMap, File rangeLog) throws IOException
-    {
+    private void applyRangeMap(InputSupplier inSup, OutputSupplier outSup, FileCollection srg, FileCollection exc, File rangeMap, Path rangeLog) throws IOException {
         RangeApplier app = new RangeApplier().readSrg(srg.getFiles());
 
-        app.setOutLogger(Constants.getTaskLogStream(getProject(), this.getName() + ".log"));
+        app.setOutLogger(Constants.getTaskLogStream(buildDir, this.getName() + ".log"));
 
-        if (!exc.isEmpty())
-        {
+        if (!exc.isEmpty()) {
             app.readParamMap(exc);
         }
 
@@ -142,19 +124,28 @@ public class ApplyS2STask extends DefaultTask
         app.remapSources(inSup, outSup, rangeMap, false);
     }
 
+    @Inject
+    protected ObjectFactory getInjectedObjectFactory() {
+        throw new IllegalStateException("must be injected");
+    }
 
-    private FileCollection generateDefaultExc(File modifiers, FileCollection currentExcs, FileCollection srgs)
-    {
+    private FileCollection createFileCollection(Object... paths) {
+        return GradleVersionUtils.choose("5.3", () -> getProject().files(paths), () -> getInjectedObjectFactory().fileCollection().from(paths));
+    }
+
+    private FileCollection createFileTree(Object baseDir) {
+        return GradleVersionUtils.choose("6.0", () -> getProject().fileTree(baseDir), () -> getInjectedObjectFactory().fileTree().from(baseDir));
+    }
+
+    private FileCollection generateDefaultExc(File modifiers, FileCollection currentExcs, FileCollection srgs) {
         if (modifiers == null || !modifiers.exists())
             return currentExcs;
 
-        Map<String, Boolean> statics = Maps.newHashMap();
+        Map<String, Boolean> statics = new HashMap<>();
 
-        try
-        {
+        try {
             getLogger().debug("  Reading Modifiers:");
-            for (String line : Files.readLines(modifiers, Charset.defaultCharset()))
-            {
+            for (String line : Files.readAllLines(modifiers.toPath(), Charset.defaultCharset())) {
                 if (Strings.isNullOrEmpty(line) || line.startsWith("#"))
                     continue;
                 String[] args = line.split("=");
@@ -168,12 +159,10 @@ public class ApplyS2STask extends DefaultTask
             temp.getParentFile().mkdirs();
             temp.createNewFile();
 
-            BufferedWriter writer = Files.newWriter(temp, Charsets.UTF_8);
-            for (File f : srgs)
-            {
+            BufferedWriter writer = Files.newBufferedWriter(temp.toPath());
+            for (File f : srgs) {
                 getLogger().debug("  Reading SRG: " + f);
-                for (String line : Files.readLines(f, Charset.defaultCharset()))
-                {
+                for (String line : Files.readAllLines(f.toPath(), Charset.defaultCharset())) {
                     if (Strings.isNullOrEmpty(line) || line.startsWith("#"))
                         continue;
 
@@ -181,27 +170,23 @@ public class ApplyS2STask extends DefaultTask
                     line = line.substring(4);
                     String[] pts = line.split(" ");
 
-                    if (type.equals("MD"))
-                    {
+                    if (type.equals("MD")) {
                         String name = pts[2].substring(pts[2].lastIndexOf('/') + 1);
-                        if (name.startsWith("func_"))
-                        {
+                        if (name.startsWith("func_")) {
                             Boolean isStatic = statics.get(pts[0] + pts[1]);
                             getLogger().debug("    MD: " + line);
                             name = name.substring(5, name.indexOf('_', 5));
 
-                            List<String> params = Lists.newArrayList();
-                            int idx = isStatic == null || !isStatic.booleanValue() ? 1 : 0;
+                            List<String> params = new ArrayList<>();
+                            int idx = isStatic == null || !isStatic ? 1 : 0;
                             getLogger().debug("      Name: " + name + " Idx: " + idx);
 
                             int i = 0;
                             boolean inArray = false;
-                            while (i < pts[1].length())
-                            {
+                            while (i < pts[1].length()) {
                                 char c = pts[1].charAt(i);
 
-                                switch (c)
-                                {
+                                switch (c) {
                                     case '(': //Start
                                         break;
                                     case ')': //End
@@ -235,14 +220,13 @@ public class ApplyS2STask extends DefaultTask
                                 i++;
                             }
 
-                            if (params.size() > 0)
-                            {
+                            if (params.size() > 0) {
                                 writer.write(pts[2].substring(0, pts[2].lastIndexOf('/')));
                                 writer.write('.');
                                 writer.write(pts[2].substring(pts[2].lastIndexOf('/') + 1));
                                 writer.write(pts[3]);
                                 writer.write("=|");
-                                writer.write(Joiner.on(",").join(params));
+                                writer.write(String.join(",", params));
                                 writer.newLine();
                             }
                         }
@@ -251,117 +235,103 @@ public class ApplyS2STask extends DefaultTask
             }
             writer.close();
 
-            List<File> files = Lists.newArrayList();
+            List<File> files = new ArrayList<>();
             files.add(temp);//Make sure the new one is first to allow others to override
             for (File f : currentExcs)
                 files.add(f);
 
-            return getProject().files(files.toArray());
+            return createFileCollection(files.toArray());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        catch (IOException e)
-        {
-            Throwables.propagate(e);
-        }
-
-        return null;
     }
 
     @InputFiles
-    public FileCollection getIns()
-    {
-        return getProject().files(in);
+    @PathSensitive(PathSensitivity.ABSOLUTE)
+    public FileCollection getIns() {
+        return createFileCollection(in);
     }
 
-    public List<File> getIn()
-    {
-        List<File> files = new LinkedList<File>();
+    @InputFiles
+    @PathSensitive(PathSensitivity.ABSOLUTE)
+    public List<File> getIn() {
+        List<File> files = new LinkedList<>();
         for (DelayedFile f : in)
             files.add(f.call());
         return files;
     }
 
-    public void addIn(DelayedFile in)
-    {
+    public void addIn(DelayedFile in) {
         this.in.add(in);
     }
 
     @OutputFiles
-    public FileCollection getOuts()
-    {
+    public FileCollection getOuts() {
         File outFile = getOut();
         if (outFile.isDirectory())
-            return getProject().fileTree(outFile);
+            return createFileTree(outFile);
         else
-            return getProject().files(outFile);
+            return createFileCollection(outFile);
     }
 
-    public File getOut()
-    {
+    @Internal
+    public File getOut() {
         return out.call();
     }
 
-    public void setOut(DelayedFile out)
-    {
+    public void setOut(DelayedFile out) {
         this.out = out;
     }
 
-    public FileCollection getSrgs()
-    {
-        return getProject().files(srg);
+    @InputFiles
+    @PathSensitive(PathSensitivity.ABSOLUTE)
+    public FileCollection getSrgs() {
+        return createFileCollection(srg);
     }
 
-    public void addSrg(DelayedFile srg)
-    {
+    public void addSrg(DelayedFile srg) {
         this.srg.add(srg);
     }
 
-    public void addSrg(String srg)
-    {
+    public void addSrg(String srg) {
         this.srg.add(srg);
     }
 
-    public void addSrg(File srg)
-    {
+    public void addSrg(File srg) {
         this.srg.add(srg);
     }
 
-    public FileCollection getExcs()
-    {
-        return getProject().files(exc);
+    @InputFiles
+    @PathSensitive(PathSensitivity.ABSOLUTE)
+    public FileCollection getExcs() {
+        return createFileCollection(exc);
     }
 
-    public void addExc(DelayedFile exc)
-    {
+    public void addExc(DelayedFile exc) {
         this.exc.add(exc);
     }
 
-    public void addExc(String exc)
-    {
+    public void addExc(String exc) {
         this.exc.add(exc);
     }
 
-    public void addExc(File exc)
-    {
+    public void addExc(File exc) {
         this.exc.add(exc);
     }
 
-    public File getRangeMap()
-    {
+    public File getRangeMap() {
         return rangeMap.call();
     }
 
-    public void setRangeMap(DelayedFile rangeMap)
-    {
+    public void setRangeMap(DelayedFile rangeMap) {
         this.rangeMap = rangeMap;
     }
 
-    public void setExcModifiers(DelayedFile value)
-    {
+    public void setExcModifiers(DelayedFile value) {
         this.excModifiers = value;
     }
 
-    public File getExcModifiers()
-    {
+    public File getExcModifiers() {
         return this.excModifiers == null ? null : this.excModifiers.call();
     }
 }
